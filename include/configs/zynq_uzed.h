c8dd7f5ce5fac4951d066b5159f1ee9e698d9ccf
Matthew Fornero <matt.fornero@mathworks.com>
2013-12-20 14:30:51 -0500

    zynq: add uzed board config

---
+++ include/configs/zynq_uzed.h
@@ -0,0 +1,34 @@
+/*
+ * (C) Copyright 2012 Xilinx
+ *
+ * Configuration for Zynq Evaluation and Development Board - ZedBoard
+ * See zynq_common.h for Zynq common configs
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#ifndef __CONFIG_ZYNQ_UZED_H
+#define __CONFIG_ZYNQ_UZED_H
+
+#define PHYS_SDRAM_1_SIZE (1024 * 1024 * 1024)
+
+#define CONFIG_ZYNQ_SERIAL_UART1
+#define CONFIG_ZYNQ_GEM0
+#define CONFIG_ZYNQ_GEM_PHY_ADDR0	0
+
+#define CONFIG_SYS_NO_FLASH
+
+#define CONFIG_ZYNQ_SDHCI0
+#define CONFIG_ZYNQ_SPI
+
+#include <configs/zynq_common.h>
+
+#endif /* __CONFIG_ZYNQ_UZED_H */
