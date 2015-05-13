d878a941b069a98472e7e2cc4c2b8508da8a205f
Michael Hennerich <michael.hennerich@analog.com>
2015-05-13 14:27:26 +0200

    include/configs/zynq_picozed_sdr.h: Fix copy and paste issues
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

--- include/configs/zynq_picozed_sdr.h
+++ include/configs/zynq_picozed_sdr.h
@@ -1,14 +1,14 @@
 /*
  * (C) Copyright 2013 Xilinx, Inc.
  *
- * Configuration for Micro Zynq Evaluation and Development Board - MicroZedBoard
+ * Configuration for PicoZed SDR Zynq Board - PicoZedSDR
  * See zynq-common.h for Zynq common configs
  *
  * SPDX-License-Identifier:	GPL-2.0+
  */
 
-#ifndef __CONFIG_ZYNQ_MICROZED_H
-#define __CONFIG_ZYNQ_MICROZED_H
+#ifndef __CONFIG_ZYNQ_PICOZED_SDR_H
+#define __CONFIG_ZYNQ_PICOZED_SDR_H
 
 #define CONFIG_SYS_SDRAM_SIZE		(1024 * 1024 * 1024)
 
@@ -27,4 +27,4 @@
 
 #include <configs/zynq-common.h>
 
-#endif /* __CONFIG_ZYNQ_MICROZED_H */
+#endif /* __CONFIG_ZYNQ_PICOZED_SDR_H*/
