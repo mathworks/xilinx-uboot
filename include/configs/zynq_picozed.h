3a14e0f5dfbf3dcd9ddbb7e715f5418476d13b14
Tom Mealey <tmealey@mathworks.com>
2018-04-06 11:53:12 -0400

    zynq: Add support for PicoZed SPL and FAT uEnv

---
+++ include/configs/zynq_picozed.h
@@ -0,0 +1,18 @@
+/*
+ * (C) Copyright 2013 Xilinx, Inc.
+ *
+ * Configuration for PicoZed Zynq Board
+ * See zynq-common.h for Zynq common configs
+ *
+ * SPDX-License-Identifier: GPL-2.0+
+ */
+
+#ifndef __CONFIG_ZYNQ_PICOZED_H
+#define __CONFIG_ZYNQ_PICOZED_H
+
+#define CONFIG_ZYNQ_I2C0
+#define CONFIG_ZYNQ_EEPROM
+
+#include <configs/zynq-common.h>
+
+#endif /* __CONFIG_ZYNQ_PICOZED_H*/
